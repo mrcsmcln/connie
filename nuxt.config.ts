ac6bab04a2a7b0d5893a6d174e3697178a70cd7e
Marcus McLean <4203694+mrcsmcln@users.noreply.github.com>
2023-05-07 22:44:41 -0400

    fix: heroicons does not need to be transpiled

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default defineNuxtConfig({
-  build: { transpile: ['luxon', '@heroicons'] },
+  build: { transpile: ['luxon'] },
   modules: ['@nuxtjs/tailwindcss'],
   tailwindcss: {
     config: {
